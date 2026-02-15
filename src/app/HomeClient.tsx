bf0e74d0ba0621fba52d326026c7892015686d1c
Dean Miller <DeanMillerNarrator@gmail.com>
2026-02-14 20:32:53 -0800

    Remove TikTok

--- src/app/HomeClient.tsx
+++ src/app/HomeClient.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import Image from "next/image";
 import Script from "next/script";
-import TikTokCarousel from './components/TikTokCarousel';
 import { useRef, useEffect, Suspense, useState } from "react";
 import { useSearchParams } from "next/navigation";
 
