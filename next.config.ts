43b968435f7d14d5b87e25b21de9d3b2021eda28
Dean Miller <DeanMillerNarrator@gmail.com>
2026-04-01 23:53:22 -0700

    Added Drag and Drop Categories

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Audit Fix: Compresses assets to reduce the total content size
-  compress: true,
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "pub-0274e76b677f47ea8135396e59f3ef10.r2.dev",
+        hostname: "pub-bc56b010ab084607b7602e2996358ca5.r2.dev",
         pathname: "/**",
       },
     ],
-    // Audit Fix: Optimizes caching for assets hosted on Cloudflare R2
-    minimumCacheTTL: 31536000, 
   },
 };
 
